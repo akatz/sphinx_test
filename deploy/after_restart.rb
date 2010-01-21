39b50d9a54c9ef557933d887316e51591e4cd8be
Dennis J. Bell <dennis.j.bell@gmail.com>
2010-01-20 17:22:18 -0800

    added debug info

--- deploy/after_restart.rb
+++ deploy/after_restart.rb
@@ -1,3 +1,11 @@
+#if node['instance_role'] == 'app'
+#  while (`curl 
+#  loop { curl master }
+#end 
+
+require 'pp'
+pp node
+
 run <<-END
   if [ -f '#{shared_path}/system/maintenance.html' ] ; then
     rm #{shared_path}/system/maintenance.html
