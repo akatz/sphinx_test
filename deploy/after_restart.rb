852bbf29b53a53d735b28b32b19c7e6765d52df8
Dennis J. Bell <dennis.j.bell@gmail.com>
2010-01-23 21:18:18 -0800

    added more debug info

--- deploy/after_restart.rb
+++ deploy/after_restart.rb
@@ -1,17 +1,19 @@
 appname = 'sphinxtest' #replace with your app's name
+
+# Sleep while the master's maintenance page is present
 node = configuration[:node]
-
+cmd = "curl -H \"Host: #{node[:applications][appname][:vhosts][0][:name]}\" #{node[:master_app_server][:private_dns_name]} -I 2>/dev/null| head -1 | awk '{print $2}'"
 if node['instance_role'] == 'app'
-  while (`curl -H "Host: #{node[:applications][appname][:vhosts][0][:name]}" #{node[:master_app_server][:private_dns_name]} -I 2>/dev/null| head -1 | awk '{print $2}'`.to_i == 503 )
-
-    Chef::Log.info(
-      "Testing: curl -H \"Host: #{node[:applications][appname][:vhosts][0][:name]}\" #{node[:master_app_server][:private_dns_name]} -I 2>/dev/null| head -1 | awk '{print $2}'"
-    )
+  while (`#{cmd}`.to_i == 503 )
+    Chef::Log.info(cmd)
     sleep 5
   end
-else
-  sleep 20
 end 
+
+require 'pp'
+Chef::Log.info(node.pretty_inspect)
+
+# Remove maintenance page if it exists.
 run <<-END
   if [ -f '#{shared_path}/system/maintenance.html' ] ; then
     rm #{shared_path}/system/maintenance.html
