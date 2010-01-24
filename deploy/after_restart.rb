812cc69719bbc1cdcf932700fcbc07514c674d1e
Dennis J. Bell <dennis.j.bell@gmail.com>
2010-01-23 20:47:54 -0800

    added slave check against master before removing maintentance page

--- deploy/after_restart.rb
+++ deploy/after_restart.rb
@@ -1,7 +1,11 @@
-#if node['instance_role'] == 'app'
-#  while (`curl 
-#  loop { curl master }
-#end 
+node = configuration[:node]
+domain = 'sphinxtest.example.com'
+
+if node['instance_role'] == 'app'
+  while (`curl -H "Host: #{domain}" #{node[:master_app_server][:private_dns_name]} -I 2>/dev/null| head -1 | awk '{print $2}'`.to_i == 503 )
+    sleep 5
+  end
+end 
 
 require 'pp'
 Chef::Log.info(configuration[:node].pretty_inspect)
