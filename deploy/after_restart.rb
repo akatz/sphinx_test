359449fe791edd86b66f62c603842ec81528b69e
Dennis J. Bell <dennis.j.bell@gmail.com>
2010-01-23 21:47:31 -0800

    more debugging stuff - take 3

--- deploy/after_restart.rb
+++ deploy/after_restart.rb
@@ -3,9 +3,10 @@ appname = shared_path.split('/')[2]
 node = configuration[:node]
 cmd = "curl -H \"Host: #{node[:applications][appname][:vhosts][0][:name]}\" #{node[:master_app_server][:private_dns_name]} -I 2>/dev/null| head -1 | awk '{print $2}'"
 if node['instance_role'] == 'app'
+  Chef::Log.info(cmd)
   while (`#{cmd}`.to_i == 503 )
-    Chef::Log.info(cmd)
     sleep 5
+    Chef::Log.info(cmd)
   end
 else
   sleep 20
