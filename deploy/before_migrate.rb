1e6f8fcf7cb829a2b520bd463d8bce40dbe74207
Christopher Stolt <stolt45@gmail.com>
2010-02-02 20:26:14 -0800

    deploy hook testing for rede

--- deploy/before_migrate.rb
+++ deploy/before_migrate.rb
@@ -1,5 +1,7 @@
 require 'erb'
 
+run "cd #{release_path} && RAILS_ENV=production rake asset:packager:build_all"
+
 appname = shared_path.split('/')[2]
 node = configuration[:node]
 Chef::Log.info("Run Migrations: #{node[:applications][appname][:run_migrations].to_s}");
