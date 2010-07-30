b2a076046a68f502027cf491c398fdb2867fbcb9
Avrohom Katz <akatz@engineyard.com>
2010-07-30 18:33:17 -0400

    sphinx index stuff

--- deploy/after_migrate.rb
+++ deploy/after_migrate.rb
@@ -1,3 +1,16 @@
+execute "update sphinx.yml for #{app.name}" do
+  command "cd /;ln -sfv /data/#{app.name}/shared/config/sphinx.yml /data/#{app.name}/current/config/sphinx.yml"
+end
+execute "sphinx config" do
+
+  command "rake ts:configure"
+  user node[:owner_name]
+  environment({
+    'HOME' => "/home/#{node[:owner_name]}",
+    'RAILS_ENV' => node[:environment][:framework_env]
+  })
+  cwd "/data/#{app.name}/current"
+end
 execute "thinking sphinx index" do
   command "rake ts:in"
   user node[:owner_name]
